ff7978e97984073ed6fed38c956012a9e1d83aea
Vamsi Nellore <vamsi.nellore@gmail.com>
2018-03-20 22:05:39 -0400

    Using props

--- app/index.js
+++ app/index.js
@@ -2,15 +2,47 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-class UserInfo extends React.Component {
+class Avatar extends React.Component {
   render() {
     return (
-      <div>Hello {this.props.name}!</div>
+      <img src={this.props.img} />
+    )
+  }
+}
+
+class Label extends React.Component {
+  render() {
+    return (
+      <h1>Name: {this.props.name}</h1>
+    )
+  }
+}
+
+class ScreenName extends React.Component {
+  render() {
+    return (
+      <h3>Username: {this.props.username}</h3>
+    )
+  }
+}
+
+class Badge extends React.Component {
+  render() {
+    return (
+      <div>
+        <Avatar img={this.props.user.img}/>
+        <Label name={this.props.user.name}/>
+        <ScreenName username={this.props.user.username}/>
+      </div>
     )
   }
 }
 
 ReactDOM.render(
-  <UserInfo name="Vamsi" />,
+  <Badge user={{
+    name: 'Vamsi Nellore',
+    img: 'https://octodex.github.com/images/labtocat.png',
+    username: 'vnellore'
+  }} />,
   document.getElementById('app')
 );
