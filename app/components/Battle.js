f519c6cdf6a9cece91922a90de007105519ae010
Vamsi Nellore <vamsi.nellore@gmail.com>
2018-04-09 07:24:29 -0400

    Added router components for navigation

---
+++ app/components/Battle.js
@@ -0,0 +1,14 @@
+var React = require('react');
+var Link = require('react-router-dom').Link;
+
+class Battle extends React.Component {
+    render() {
+        return (
+            <div>
+                Battle!
+            </div>
+        )
+    }
+}
+
+module.exports = Battle;
