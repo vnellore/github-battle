8263259690bff4f90275f9ec05a67d7cfbd15692
Vamsi Nellore <vamsi.nellore@gmail.com>
2018-05-01 20:51:10 -0400

    Removed constructors from classes

--- app/components/Loading.js
+++ app/components/Loading.js
@@ -1,57 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from 'react'
+import PropTypes from 'prop-types'
 
 const styles = {
-    content: {
-        textAlign: 'center',
-        fontSize: '35px'
-    }
-}
+  content: {
+    textAlign: 'center',
+    fontSize: '35px'
+  }
+};
 
 class Loading extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            text: props.text,
-            speed: props.speed
-        };
-    }
-
-    componentDidMount() {
-        const { text, speed } = this.props;
-        const stopper = `${text}...`;
-        this.interval = window.setInterval(() => {
-            this.state.text === stopper
-                ? this.setState(() => text)
-                : this.setState((prevState) => ({ text: prevState.text + '.' }));
-
-        }, speed)
-
-    }
-
-    componentWillUnmount() {
-        console.log('CLEAR THE INTERVAL');
-        window.clearInterval(this.interval);
-    }
-
-    render() {
-        return (
-            <p style={styles.content}>
-                {this.state.text}
-            </p>
-        )
-    }
-}
-
-Loading.propTypes = {
+  static propTypes = {
     text: PropTypes.string.isRequired,
-    speed: PropTypes.number.isRequired
-}
-
-Loading.defaultProps = {
+    speed: PropTypes.number.isRequired,
+  }
+  static defaultProps = {
     text: 'Loading',
     speed: 300
-};
+  }
+  state = {
+    text: this.props.text
+  }
+  componentDidMount() {
+    const { text, speed } = this.props
+    const stopper = text + '...';
+
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState(() => ({ text: this.props.text }))
+        : this.setState((prevState) => ({ text: prevState.text + '.' }))
+    }, speed)
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+  render() {
+    return (
+      <p style={styles.content}>
+        {this.state.text}
+      </p>
+    )
+  }
+}
 
 export default Loading;
